432054946c689c79a94384475ad979e90639707d
bravodiego1 <dibravo@frba.utn.edu.ar>
2025-04-29 15:07:42 -0300

    mas funciones (a corregir seguro)

--- src/Lib.hs
+++ src/Lib.hs
@@ -28,12 +28,24 @@ personajePamela = UnPersonaje {
     cantidadDeVida = 9600
 }
 
+nombres :: Personaje -> String
+nombres personaje = nombre personaje
+
 restandoVida :: Int -> Int
-restandoVida vida = (-1000) vida
+restandoVida vida 
+    | vida - 1000 > 0 = vida - 1000
+    | vida - 1000 < 0 = 0
 
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa personaje = personaje {cantidadDeVida = restandoVida (cantidadDeVida personaje) } 
 
+lluviaDeTuercas :: Personaje -> String -> Personaje 
+lluviaDeTuercas personaje mismoEquipo 
+    | mismoEquipo == "mismo equipo" = personaje {cantidadDeVida = (cantidadDeVida personaje) + 800}
+    | mismoEquipo == "distinto equipo" = personaje {cantidadDeVida = (cantidadDeVida personaje) - div (cantidadDeVida personaje) 2}
 
 
-
+enLasUltimas :: Personaje -> String
+enLasUltimas personaje
+    | cantidadDeVida personaje < 800 = "El personaje esta en las ultimas"
+    | otherwise = "El personaje no esta en las ultimas"
