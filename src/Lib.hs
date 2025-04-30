caafd2d5dc0f8e2f56114acc13a8a2f5024e4afa
bravodiego1 <dibravo@frba.utn.edu.ar>
2025-04-30 01:57:02 -0300

    falta funcion poder especial, SEGUIR MEJORANDO EL CODIGO

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,31 +5,28 @@ data Personaje = UnPersonaje {
     nombre :: String, 
     poderBasico :: String, 
     superPoder :: String,
-    poderActivo :: Bool,
+    superPoderActivo :: Bool,
     cantidadDeVida :: Int
     } deriving Show
 
 
-personajeEspina :: Personaje
-personajeEspina = UnPersonaje {
+espina :: Personaje
+espina = UnPersonaje {
     nombre = "Espina",
     poderBasico= "Bola de espinas",
     superPoder ="Granada de espinas",
-    poderActivo = True,
+    superPoderActivo = True,
     cantidadDeVida = 4800
 }
 
-personajePamela :: Personaje
-personajePamela = UnPersonaje {
+pamela :: Personaje
+pamela = UnPersonaje {
     nombre = "Pamela",
     poderBasico= "Lluvia de tuercas",
     superPoder ="Torre curativa",
-    poderActivo = False,
+    superPoderActivo = True,
     cantidadDeVida = 9600
 }
-
-nombres :: Personaje -> String
-nombres personaje = nombre personaje
 
 restandoVida :: Int -> Int -> Int
 restandoVida vida danio = max (vida - danio) 0
@@ -37,16 +34,47 @@ restandoVida vida danio = max (vida - danio) 0
 hacerDanio :: Personaje -> Int -> Personaje
 hacerDanio contrincante danio = contrincante {cantidadDeVida  = restandoVida (cantidadDeVida contrincante) danio}
 
+sumandoVida :: Int -> Int -> Int
+sumandoVida vida agregado = (+vida) agregado
+
+vidaAequipo :: Personaje -> Int -> Personaje
+vidaAequipo compaDeEquipo agregado = compaDeEquipo {cantidadDeVida = sumandoVida (cantidadDeVida compaDeEquipo) agregado}
+
+-- TRATAR DE JUNTAR hacerDanio Y vidaAequipo PARA NO REPETIR LOGICA
+
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa contrincante = hacerDanio contrincante 1000
 
-lluviaDeTuercas :: Personaje -> String -> Personaje 
-lluviaDeTuercas contrincante mismoEquipo 
-    | mismoEquipo == "mismo equipo" = contrincante {cantidadDeVida = (cantidadDeVida contrincante) + 800}
-    | mismoEquipo == "distinto equipo" = contrincante {cantidadDeVida = (cantidadDeVida contrincante) - div (cantidadDeVida contrincante) 2}
+lluviaDeTuercas :: Personaje -> Bool -> Personaje 
+lluviaDeTuercas jugador mismoEquipo 
+    | mismoEquipo = vidaAequipo jugador 800
+    | otherwise = hacerDanio jugador (div (cantidadDeVida jugador) 2) 
 
+granadaDeEspinas :: Personaje -> Int -> Personaje
+granadaDeEspinas contrincante radio
+    | radio > 3 && cantidadDeVida contrincante < 800 =
+      hacerDanio (contrincante {
+          nombre = nombre contrincante ++ " ...Espina estuvo aqui",
+          superPoderActivo = False
+      }) (cantidadDeVida contrincante)
+    | radio > 3 = contrincante {nombre = nombre contrincante ++ " ...Espina estuvo aqui"}
+    | otherwise = bolaEspinosa contrincante
+
+torreCurativa :: Personaje -> Personaje
+torreCurativa compaDeEquipo = vidaAequipo compaDeEquipo (cantidadDeVida compaDeEquipo)
+
+
+ataquePoderEspecialEspina :: Personaje -> Int -> Personaje
+ataquePoderEspecialEspina contrincante radio
+    | superPoderActivo contrincante = (bolaEspinosa.granadaDeEspinas contrincante) radio
+    | otherwise = bolaEspinosa contrincante 
+
+-- ataquePoderEspecialPamela :: Personaje -> Personaje
+-- ataquePoderEspecialPamela jugador =
 
 enLasUltimas :: Personaje -> String
 enLasUltimas personaje
     | cantidadDeVida personaje < 800 = "El personaje esta en las ultimas"
     | otherwise = "El personaje no esta en las ultimas"
+
+
